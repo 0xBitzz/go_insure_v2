6c9aabe8ffeee253a24c870296d6eca3f2dad4a0
0xBitzz <successdaudu@gmail.com>
2024-01-06 17:00:33 +0100

    added README

--- backend/smart_contracts/go_insure/helpers/constants.py
+++ backend/smart_contracts/go_insure/helpers/constants.py
@@ -7,7 +7,7 @@ REJECTED = Bytes("Rejected")
 # TODO: Premium Amount should be made dynamic due to factors such as age, health condition e.t.c.
 # TODO: Algorithm to calculate premium & coverage amount respectively.
 # TODO: Support payment with EURO.
-PREMIUM_AMOUNT = Int(1_000_000)  # 1 USDC
-COVERAGE_AMOUNT = Int(5_000_000)  # 5 USDC
+PREMIUM_AMOUNT = Int(1_000_000)  # 1A
+COVERAGE_AMOUNT = Int(5_000_000)  # 5A
 
 DEFAULT_POLICY_EXPIRATION_TIMELINE = Int(31_536_000)
